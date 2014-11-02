3910bf5e466dc3908e9de8687e6b3166b7e33975
walshification <walshification@gmail.com>
2014-11-02 13:53:46 -0600

    added 404

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,6 +1,12 @@
 class VisitsController < ApplicationController
   def create
-    @visit = Link.find_by(:slug => params[:slug]).visits.create(:ip_address => request.remote_ip)
-    redirect_to "http://#{@link.target_url}"
+    @visit = Link.find_by(:slug => params[:slug])
+
+    if @link
+      @link.visits.create(:ip_address => request.remote_ip)
+      redirect_to "http://#{@link.target_url}"
+    else
+      raise ActionController::RoutingError.new('Not Found')
+    end
   end
 end
